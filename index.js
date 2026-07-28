353da4c39ba5d004b3103c42cdaf7345c0278d0c
Bharath N <bharath.n.offical@gmail.com>
2026-07-29 00:02:14 +0530

    CHORE: Remove unused code and comments from index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-/* ==========================================================
-Buddy - Markdown Based local work tracker
-* ==========================================================
-
-const STATUS_OPTIONS = ["Planning", "In Progress", "On Hold", "Done"];
-const PIPELINE_STATUS_OPTIONS = ["Dev", "QA", "UAT", "Production"];
